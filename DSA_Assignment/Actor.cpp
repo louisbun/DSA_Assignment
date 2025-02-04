d988e907a6e4ffbb8d5474160eeaacb091bb7bab
Chong Yun Ze <S10257927@connect.np.edu.sg>
2025-02-04 13:04:13 +0800

    Menu

--- DSA_Assignment/Actor.cpp
+++ DSA_Assignment/Actor.cpp
@@ -1,1 +1,21 @@
 #include "Actor.h"
+
+Actor::Actor() {
+    id = 0;
+    name = "unknown";
+    birth = 0;
+}
+
+Actor::Actor(int id, string name, int birth) {
+    this->id = id;
+    this->name = name;
+    this->birth = birth;
+}
+
+int Actor::getId() { return id; }
+string Actor::getName() { return name; }
+int Actor::getBirth() { return birth; }
+
+void Actor::setId(int id) { this->id = id; }
+void Actor::setName(string name) { this->name = name; }
+void Actor::setBirth(int birth) { this->birth = birth; }
--- DSA_Assignment/DSA_Assignment.cpp
+++ DSA_Assignment/DSA_Assignment.cpp
@@ -1,21 +1,96 @@
-// DSA_Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int displayMenu() {
+    int choice;
+
+    cout << "\n------------------- Menu ---------------------------------------";
+    cout << "\n[1]  Add a new actor";
+    cout << "\n[2]  Add new movie";
+    cout << "\n[3]  Add an actor to a movie";
+    cout << "\n[4]  Update actor details";
+    cout << "\n[5]  Update movie details";
+    cout << "\n[6]  Display actors according to age";
+    cout << "\n[7]  Display mvoies made within the past 3 years";
+    cout << "\n[8]  Display all movies an actor starred in";
+    cout << "\n[9]  Display all actors in a movie";
+    cout << "\n[10] Display a list of all actors that a particular actor knows";
+    cout << "\n[0]  Exit";
+    cout << "\n----------------------------------------------------------------";
+    cout << "\nChoose an option: ";
+
+    cin >> choice;
+    cin.ignore(); // To clear newline from input buffer
+
+    return choice;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    int choice;
+    do {
+        choice = displayMenu();  // Show menu and get the user's choice
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        if (choice == 1) 
+        {
+            
+        }
+
+        else if (choice == 2) 
+        {
+            
+        }
+
+        else if (choice == 3) 
+        {
+            
+        }
+
+        else if (choice == 4) 
+        {
+            
+        }
+
+        else if (choice == 5) 
+        {
+            
+        }
+
+        else if (choice == 6) 
+        {
+
+        }
+
+        else if (choice == 7)
+        {
+
+        }
+
+        else if (choice == 8)
+        {
+
+        }
+
+        else if (choice == 9)
+        {
+
+        }
+
+        else if (choice == 10)
+        {
+
+        }
+
+        else 
+        {
+            cout << "Invalid choice." << endl;
+        }
+
+    } while (choice != 0);  // Continue until the user chooses to exit
+    
+    cout << "\nExiting ..." << endl;
+
+    return 0;
+}
+
+
